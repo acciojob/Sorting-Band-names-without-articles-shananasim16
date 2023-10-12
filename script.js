77f177319306df9b43ea86c58f6339f0183b7ec3
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-12 22:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here
+document.addEventListener('DOMContentLoaded', function() {
+    const bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+    // Function to remove articles ('a', 'an', 'the') and sort the band names
+    function sortBandNames(names) {
+        return names
+            .map(name => name.replace(/^(a|an|the) /i, '').trim()) // Remove articles
+            .sort(); // Sort alphabetically
+    }
+
+    // Get the sorted band names
+    const sortedNames = sortBandNames(bandNames);
+
+    // Get the ul element to display the sorted names
+    const ul = document.getElementById('band');
+
+    // Create and append li elements to the ul
+    sortedNames.forEach(name => {
+        const li = document.createElement('li');
+        li.textContent = name;
+        ul.appendChild(li);
+    });
+});
